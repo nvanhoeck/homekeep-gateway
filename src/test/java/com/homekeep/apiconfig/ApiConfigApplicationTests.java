40ea34f6f5308a6043d863f578f5ef97f8a84ea5
Niko VH <niko.vanhoeck@gmail.com>
2020-05-30 12:24:32 +0200

    [HOME]: Init

---
+++ src/test/java/com/homekeep/apiconfig/ApiConfigApplicationTests.java
@@ -0,0 +1,26 @@
+package com.homekeep.apiconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class ApiConfigApplicationTests {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void checkHealth() {
+        ResponseEntity<String> responseEntity = this.testRestTemplate.getForEntity("http://localhost:"+port+"/actuator/health", String.class);
+		assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
